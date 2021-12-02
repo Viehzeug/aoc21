4cb2030ab23a33f1e9e3b2cbd37be86e9eb0d960
Marc Fischer <mail@marcfischer.at>
2021-12-02 09:28:50 +0100

    day 2

---
+++ 2/main.py
@@ -0,0 +1,32 @@
+hpos = 0
+depth = 0
+with open('input', 'r') as f:
+    for line in f:
+        direction, distance = line.strip().split(' ')
+        distance = int(distance)
+        if direction == 'forward':
+            hpos += distance
+        elif direction == 'up':
+            depth -= distance
+        elif direction == 'down':
+            depth += distance
+print('Task 1:', hpos * depth)
+
+
+
+hpos = 0
+depth = 0
+aim = 0
+with open('input', 'r') as f:
+    for line in f:
+        direction, distance = line.strip().split(' ')
+        distance = int(distance)
+        if direction == 'forward':
+            hpos += distance
+            depth +=  aim * distance
+        elif direction == 'up':
+            aim -= distance
+        elif direction == 'down':
+            aim += distance
+print('Task 2:', hpos * depth)
+
